b9d3847ff27bf7baecffe89d3151ded9d234d036
it24103951 <it24103951@my.sliit.lk>
2025-05-13 00:32:05 +0530

    init commit

---
+++ src/main/java/com/tutor_buddy/TutorBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.tutor_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutor_buddy/TutorBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutor_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
