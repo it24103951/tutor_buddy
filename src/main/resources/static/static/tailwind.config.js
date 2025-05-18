c0283a1242c1fa8428e07e5970ae6ebb47479dc9
IT24104104 <IT24104104@my.sliit.lk>
2025-05-18 21:16:45 +0530

    Add files via upload

---
+++ src/main/resources/static/static/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/main/resources/static/**/*.html",
+    "./src/main/resources/static/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
